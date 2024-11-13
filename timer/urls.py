224b477b1db14ba72a1c26c0803a1709386c81dd
Nicolas <nicolas.robouam@gmail.com>
2024-11-13 23:00:17 +0100

    connection des différent page HTML

--- timer/urls.py
+++ timer/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, ProfilPageView, TimerSettingPageView, TimerPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name ="home"),
-]
+    path('profil/', ProfilPageView.as_view(), name ="profil"),
+    path('timer_setting/', TimerSettingPageView.as_view(), name ="timer_setting"),
+    path('timer/', TimerPageView.as_view(), name ="timer"),
+] 
